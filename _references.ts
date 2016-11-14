ba842df2d9a4b1b2cd282fa7b3e0849339561e18
lord-saumagen <lord.saumagen@gmail.com>
2016-11-14 02:48:54 +0100

    Change solution setting.

--- _references.ts
+++ _references.ts
@@ -1,3 +1,3 @@
-﻿/// <reference path="../TS_Base/TS_Base.d.ts" />
+﻿/// <reference path="../TypeScript-Base/TS-Base.d.ts" />
 /// <reference path="./Scripts/qunit.d.ts" />
 /// <reference path="./Scripts/jquery.d.ts" />
